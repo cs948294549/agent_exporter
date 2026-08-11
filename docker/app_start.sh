21dc9da54fe23ce564d212beb558a1e2a55f0bdb
weidian <weidian@weidiandeMacBook-Pro.local>
2026-08-11 14:14:26 +0800

    fix vender

--- docker/app_start.sh
+++ docker/app_start.sh
@@ -1,4 +1,5 @@
 docker run -d -p 8000:5000 \
+  -e PYTHONUNBUFFERED=1 \
   -v /root/docker_apps/agent_exporter/logs:/app/logs \
   -v /root/docker_apps/agent_exporter/configs:/app/configs \
   --name exporter agent-exporter:v1
